18af1837e45423cc9360c6a31a074e46068488b8
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2020-01-09 21:17:47 +0100

    Update CustomerController with data model

--- src/main/java/spring/mvc/app/controller/CustomerController.java
+++ src/main/java/spring/mvc/app/controller/CustomerController.java
@@ -1,15 +1,27 @@
 package spring.mvc.app.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.mvc.app.dao.CustomerDAO;
+import spring.mvc.app.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+  @Autowired
+  private CustomerDAO customerDAO;
+
   @RequestMapping("/list")
   public String listCustomers(Model model){
+
+    List<Customer> theCustomers = customerDAO.getCustomers();
+
+    model.addAttribute("customers", theCustomers);
+
     return "list-customers";
   }
 }
